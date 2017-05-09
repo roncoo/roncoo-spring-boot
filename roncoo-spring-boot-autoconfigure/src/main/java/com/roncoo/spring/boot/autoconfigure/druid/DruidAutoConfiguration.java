9e4313a8e614b6b906f45e6917ee31d41d8bc9c7
wujing <297115770@qq.com>
2017-05-09 19:36:26 +0800

    修改bug

--- roncoo-spring-boot-autoconfigure/src/main/java/com/roncoo/spring/boot/autoconfigure/druid/DruidAutoConfiguration.java
+++ roncoo-spring-boot-autoconfigure/src/main/java/com/roncoo/spring/boot/autoconfigure/druid/DruidAutoConfiguration.java
@@ -1,11 +1,12 @@
 package com.roncoo.spring.boot.autoconfigure.druid;
 
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.jdbc.DatabaseDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,13 +27,77 @@ public class DruidAutoConfiguration {
 
 	@Bean
 	public DruidDataSource dataSource() {
-		DruidDataSource druidDataSource = (DruidDataSource) properties.initializeDataSourceBuilder().type(DruidDataSource.class).build();
+		/*DruidDataSource druidDataSource = (DruidDataSource) properties.initializeDataSourceBuilder().type(DruidDataSource.class).build();
 		DatabaseDriver databaseDriver = DatabaseDriver.fromJdbcUrl(properties.determineUrl());
 		String validationQuery = databaseDriver.getValidationQuery();
 		if (validationQuery != null) {
 			druidDataSource.setValidationQuery(validationQuery);
 		}
-		return druidDataSource;
+		return druidDataSource;*/
+
+		DruidDataSource dataSource = new DruidDataSource();
+		dataSource.setUrl(properties.getUrl());
+		dataSource.setUsername(properties.getUsername());
+		dataSource.setPassword(properties.getPassword());
+		
+		if(properties.getName() != null){
+			dataSource.setName(properties.getName());	
+		}
+		if(properties.getDriverClass() != null){
+			dataSource.setDriverClassName(properties.getDriverClass());	
+		}
+		if (properties.getInitialSize() > 0) {
+			dataSource.setInitialSize(properties.getInitialSize());
+		}
+		if (properties.getMinIdle() > 0) {
+			dataSource.setMinIdle(properties.getMinIdle());
+		}
+		if (properties.getMaxActive() > 0) {
+			dataSource.setMaxActive(properties.getMaxActive());
+		}
+		if (properties.getMaxWait() > 0) {
+			dataSource.setMaxWait(properties.getMaxWait());
+		}
+		if (properties.getPoolPreparedStatements() != null) {
+			dataSource.setPoolPreparedStatements(properties.getPoolPreparedStatements());
+		}
+		if (properties.getMaxPoolPreparedStatementPerConnectionSize() > 0) {
+			dataSource.setMaxPoolPreparedStatementPerConnectionSize(properties.getMaxPoolPreparedStatementPerConnectionSize());
+		}
+		if (properties.getValidationQuery() != null) {
+			dataSource.setValidationQuery(properties.getValidationQuery());
+		}
+		if (properties.getTestOnBorrow() != null) {
+			dataSource.setTestOnBorrow(properties.getTestOnBorrow());
+		}
+		if (properties.getTestWhileIdle() != null) {
+			dataSource.setTestWhileIdle(properties.getTestWhileIdle());
+		}
+		if (properties.getTestOnReturn() != null) {
+			dataSource.setTestOnReturn(properties.getTestOnReturn());
+		}
+		if (properties.getConnectionProperties() != null) {
+			dataSource.setConnectionProperties(properties.getConnectionProperties());
+		}
+		if (properties.getFilters() != null) {
+			try {
+				dataSource.setFilters(properties.getFilters());
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if (properties.getMinEvictableIdleTimeMillis() > 0) {
+			dataSource.setMinEvictableIdleTimeMillis(properties.getMinEvictableIdleTimeMillis());
+		}
+		if (properties.getTimeBetweenEvictionRunsMillis() > 0) {
+			dataSource.setTimeBetweenEvictionRunsMillis(properties.getTimeBetweenEvictionRunsMillis());
+		}
+		try {
+			dataSource.init();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return dataSource;
 	}
 
 }
--- roncoo-spring-boot-autoconfigure/src/main/java/com/roncoo/spring/boot/autoconfigure/druid/DruidProperties.java
+++ roncoo-spring-boot-autoconfigure/src/main/java/com/roncoo/spring/boot/autoconfigure/druid/DruidProperties.java
@@ -1,6 +1,5 @@
 package com.roncoo.spring.boot.autoconfigure.druid;
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
@@ -8,55 +7,61 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * @author wujing
  */
 @ConfigurationProperties(prefix = "spring.datasource.druid")
-public class DruidProperties extends DataSourceProperties{
+public class DruidProperties {
 
+	private String name;
 	private String url;
 	private String username;
 	private String password;
 	private String driverClass;
+
+	private int initialSize = 0;
+	private int maxActive = 8;
+	private int minIdle = 8;
+	private int maxWait;
+
+	private Boolean poolPreparedStatements = false;
+	private int maxPoolPreparedStatementPerConnectionSize = -1;
+
+	private String validationQuery;
+	private Boolean testOnBorrow = true;
+	private Boolean testOnReturn = false;
+	private Boolean testWhileIdle = false;
+
 	private String filters;
 	private String connectionProperties;
-	private String validationQuery;
 
-	private Boolean testWhileIdle;
-	private Boolean testOnBorrow;
-	private Boolean testOnReturn;
-	private Boolean poolPreparedStatements;
+	private int timeBetweenEvictionRunsMillis = 1;
+	private int minEvictableIdleTimeMillis;
 
-	private Integer maxWait;
-	private Integer maxActive;
-	private Integer minIdle;
-	private Integer initialSize;
-	private Integer timeBetweenEvictionRunsMillis;
-	private Integer minEvictableIdleTimeMillis;
-	private Integer maxPoolPreparedStatementPerConnectionSize;
+	public String getName() {
+		return name;
+	}
 
-	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getUrl() {
 		return url;
 	}
 
-	@Override
 	public void setUrl(String url) {
 		this.url = url;
 	}
 
-	@Override
 	public String getUsername() {
 		return username;
 	}
 
-	@Override
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
-	@Override
 	public String getPassword() {
 		return password;
 	}
 
-	@Override
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -69,20 +74,52 @@ public class DruidProperties extends DataSourceProperties{
 		this.driverClass = driverClass;
 	}
 
-	public String getFilters() {
-		return filters;
+	public int getInitialSize() {
+		return initialSize;
 	}
 
-	public void setFilters(String filters) {
-		this.filters = filters;
+	public void setInitialSize(int initialSize) {
+		this.initialSize = initialSize;
 	}
 
-	public String getConnectionProperties() {
-		return connectionProperties;
+	public int getMaxActive() {
+		return maxActive;
 	}
 
-	public void setConnectionProperties(String connectionProperties) {
-		this.connectionProperties = connectionProperties;
+	public void setMaxActive(int maxActive) {
+		this.maxActive = maxActive;
+	}
+
+	public int getMinIdle() {
+		return minIdle;
+	}
+
+	public void setMinIdle(int minIdle) {
+		this.minIdle = minIdle;
+	}
+
+	public int getMaxWait() {
+		return maxWait;
+	}
+
+	public void setMaxWait(int maxWait) {
+		this.maxWait = maxWait;
+	}
+
+	public Boolean getPoolPreparedStatements() {
+		return poolPreparedStatements;
+	}
+
+	public void setPoolPreparedStatements(Boolean poolPreparedStatements) {
+		this.poolPreparedStatements = poolPreparedStatements;
+	}
+
+	public int getMaxPoolPreparedStatementPerConnectionSize() {
+		return maxPoolPreparedStatementPerConnectionSize;
+	}
+
+	public void setMaxPoolPreparedStatementPerConnectionSize(int maxPoolPreparedStatementPerConnectionSize) {
+		this.maxPoolPreparedStatementPerConnectionSize = maxPoolPreparedStatementPerConnectionSize;
 	}
 
 	public String getValidationQuery() {
@@ -91,14 +128,6 @@ public class DruidProperties extends DataSourceProperties{
 
 	public void setValidationQuery(String validationQuery) {
 		this.validationQuery = validationQuery;
-	}
-
-	public Boolean getTestWhileIdle() {
-		return testWhileIdle;
-	}
-
-	public void setTestWhileIdle(Boolean testWhileIdle) {
-		this.testWhileIdle = testWhileIdle;
 	}
 
 	public Boolean getTestOnBorrow() {
@@ -117,67 +146,44 @@ public class DruidProperties extends DataSourceProperties{
 		this.testOnReturn = testOnReturn;
 	}
 
-	public Boolean getPoolPreparedStatements() {
-		return poolPreparedStatements;
+	public Boolean getTestWhileIdle() {
+		return testWhileIdle;
 	}
 
-	public void setPoolPreparedStatements(Boolean poolPreparedStatements) {
-		this.poolPreparedStatements = poolPreparedStatements;
+	public void setTestWhileIdle(Boolean testWhileIdle) {
+		this.testWhileIdle = testWhileIdle;
 	}
 
-	public Integer getMaxWait() {
-		return maxWait;
+	public String getFilters() {
+		return filters;
 	}
 
-	public void setMaxWait(Integer maxWait) {
-		this.maxWait = maxWait;
+	public void setFilters(String filters) {
+		this.filters = filters;
 	}
 
-	public Integer getMaxActive() {
-		return maxActive;
+	public String getConnectionProperties() {
+		return connectionProperties;
 	}
 
-	public void setMaxActive(Integer maxActive) {
-		this.maxActive = maxActive;
+	public void setConnectionProperties(String connectionProperties) {
+		this.connectionProperties = connectionProperties;
 	}
 
-	public Integer getMinIdle() {
-		return minIdle;
-	}
-
-	public void setMinIdle(Integer minIdle) {
-		this.minIdle = minIdle;
-	}
-
-	public Integer getInitialSize() {
-		return initialSize;
-	}
-
-	public void setInitialSize(Integer initialSize) {
-		this.initialSize = initialSize;
-	}
-
-	public Integer getTimeBetweenEvictionRunsMillis() {
+	public int getTimeBetweenEvictionRunsMillis() {
 		return timeBetweenEvictionRunsMillis;
 	}
 
-	public void setTimeBetweenEvictionRunsMillis(Integer timeBetweenEvictionRunsMillis) {
+	public void setTimeBetweenEvictionRunsMillis(int timeBetweenEvictionRunsMillis) {
 		this.timeBetweenEvictionRunsMillis = timeBetweenEvictionRunsMillis;
 	}
 
-	public Integer getMinEvictableIdleTimeMillis() {
+	public int getMinEvictableIdleTimeMillis() {
 		return minEvictableIdleTimeMillis;
 	}
 
-	public void setMinEvictableIdleTimeMillis(Integer minEvictableIdleTimeMillis) {
+	public void setMinEvictableIdleTimeMillis(int minEvictableIdleTimeMillis) {
 		this.minEvictableIdleTimeMillis = minEvictableIdleTimeMillis;
 	}
 
-	public Integer getMaxPoolPreparedStatementPerConnectionSize() {
-		return maxPoolPreparedStatementPerConnectionSize;
-	}
-
-	public void setMaxPoolPreparedStatementPerConnectionSize(Integer maxPoolPreparedStatementPerConnectionSize) {
-		this.maxPoolPreparedStatementPerConnectionSize = maxPoolPreparedStatementPerConnectionSize;
-	}
 }
