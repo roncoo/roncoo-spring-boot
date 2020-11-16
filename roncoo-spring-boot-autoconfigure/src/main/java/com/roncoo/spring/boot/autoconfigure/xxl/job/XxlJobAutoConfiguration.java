177e48468764426e43fdf343bf560c2240884faf
wujing <297115770@qq.com>
2020-11-16 20:18:03 +0800

    修复xxl-job本地启动产生端口冲突的bug

--- roncoo-spring-boot-autoconfigure/src/main/java/com/roncoo/spring/boot/autoconfigure/xxl/job/XxlJobAutoConfiguration.java
+++ roncoo-spring-boot-autoconfigure/src/main/java/com/roncoo/spring/boot/autoconfigure/xxl/job/XxlJobAutoConfiguration.java
@@ -1,15 +1,13 @@
 package com.roncoo.spring.boot.autoconfigure.xxl.job;
 
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import com.xxl.job.core.handler.annotation.XxlJob;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
-import com.xxl.job.core.handler.annotation.XxlJob;
-
 /**
- * 
  * @author wujing
  */
 @Configuration
@@ -17,23 +15,23 @@ import com.xxl.job.core.handler.annotation.XxlJob;
 @EnableConfigurationProperties(XxlJobProperties.class)
 public class XxlJobAutoConfiguration {
 
-	private final XxlJobProperties properties;
+    private final XxlJobProperties properties;
 
-	public XxlJobAutoConfiguration(XxlJobProperties properties) {
-		this.properties = properties;
-	}
+    public XxlJobAutoConfiguration(XxlJobProperties properties) {
+        this.properties = properties;
+    }
 
-	@Bean(initMethod = "start", destroyMethod = "destroy")
-	public XxlJobSpringExecutor xxlJobExecutor() {
-		XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
-		xxlJobSpringExecutor.setAdminAddresses(properties.getAdmin().getAddresses());
-		xxlJobSpringExecutor.setAppName(properties.getExecutor().getAppName());
-		xxlJobSpringExecutor.setIp(properties.getExecutor().getIp());
-		xxlJobSpringExecutor.setPort(properties.getExecutor().getPort());
-		xxlJobSpringExecutor.setAccessToken(properties.getAccessToken());
-		xxlJobSpringExecutor.setLogPath(properties.getExecutor().getLogPath());
-		xxlJobSpringExecutor.setLogRetentionDays(properties.getExecutor().getLogRetentionDays());
-		return xxlJobSpringExecutor;
-	}
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(properties.getAdmin().getAddresses());
+        xxlJobSpringExecutor.setAppName(properties.getExecutor().getAppName());
+        xxlJobSpringExecutor.setIp(properties.getExecutor().getIp());
+        xxlJobSpringExecutor.setPort(properties.getExecutor().getPort());
+        xxlJobSpringExecutor.setAccessToken(properties.getAccessToken());
+        xxlJobSpringExecutor.setLogPath(properties.getExecutor().getLogPath());
+        xxlJobSpringExecutor.setLogRetentionDays(properties.getExecutor().getLogRetentionDays());
+        return xxlJobSpringExecutor;
+    }
 
 }
